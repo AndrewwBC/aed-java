f360c5ac8304be0d38813862b84011a74f0f603b
AndrewwBC <andrew-hot2011@hotmail.com>
2025-04-24 16:30:21 -0300

    fix(stack): remove atribute

--- stack/src/main/java/org/example/App.java
+++ stack/src/main/java/org/example/App.java
@@ -2,10 +2,12 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-
-        Node node = new Node(1, null);
+        System.out.println("Hello world!");
 
         Stack stack = new Stack();
+        Node node = new Node(1, null);
+
+        stack.insertNode(node);
 
     }
 }
--- stack/src/main/java/org/example/Stack.java
+++ stack/src/main/java/org/example/Stack.java
@@ -2,19 +2,16 @@ package org.example;
 
 public class Stack implements StackBehavior{
     private Node top = null;
-    private int stackCapacity;
 
     @Override
     public boolean isStackEmpty() {
         return this.top == null;
     }
-
     @Override
     public void insertNode(Node node) {
         // should verify if stack is full
         this.top = node;
     }
-
     @Override
     public Node removeNode(Node node) {
         this.top = this.top.getNext();
