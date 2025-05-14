56240ab27ccb51a668fff80c29431b6e02b0cd11
AndrewwBC <andrew-hot2011@hotmail.com>
2025-05-14 12:45:11 -0300

    add(hashing): interface

--- hashing/src/main/java/org/example/HashTable/HashTable.java
+++ hashing/src/main/java/org/example/HashTable/HashTable.java
@@ -1,12 +1,20 @@
 package org.example.HashTable;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class HashTable {
     private Map<Integer, HashTableElements> elements = new HashMap<>();
 
+    public void addList(int k, HashTableElements list) {
+        this.elements.put(k, list);
+    }
 
+    public HashTableElements getList(int k) {
+        return this.elements.get(k);
+    }
+
+    public void removeList(int k) {
+        this.elements.remove(k);
+    }
 }
---
+++ hashing/src/main/java/org/example/Interfaces/HashFunctions.java
@@ -0,0 +1,9 @@
+package org.example.Interfaces;
+
+public interface HashFunctions {
+
+    public int divisionMethod(int k, int m);
+
+    public int multiplicationMethod(int k, int m);
+
+}
