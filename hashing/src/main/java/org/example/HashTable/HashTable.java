6a8b151a7e19ad518a5185d5f128d0d8fef40c51
AndrewwBC <andrew-hot2011@hotmail.com>
2025-05-14 12:29:40 -0300

    fix: change list to map

--- hashing/src/main/java/org/example/HashTable/HashTable.java
+++ hashing/src/main/java/org/example/HashTable/HashTable.java
@@ -1,8 +1,12 @@
 package org.example.HashTable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HashTable {
-    private List<HashTableElements> elements = new ArrayList<>();
+    private Map<Integer, HashTableElements> elements = new HashMap<>();
+
+
 }
